3c9d6a6ddb054c6678dd0be5bade3ba6e9c731fc
v_yuzhuang01 <v_yuzhuang01@baidu.com>
2019-07-30 16:46:14 +0800

    add smooth-scroll info-section

---
+++ frontend/src/components/InfoSectionPanel/InfoSectionPanel.vue
@@ -0,0 +1,33 @@
+<template>
+    <div
+        class="section">
+        <h1 class="title">{{title}}</h1>
+        <p class="line"></p>
+        <p class="description"></p>
+        <div class="content-section">{{content}}</div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        title: String,
+        content: String
+    }
+}
+</script>
+<style type="text/less" lang="less">
+    .section {
+        .title {
+
+        }
+        .line {
+
+        }
+        .description {
+
+        }
+        .content-section {
+
+        }
+    }
+</style>
